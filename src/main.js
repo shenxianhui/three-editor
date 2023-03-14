13f139ce3f9aedb114f9f9e06c034eb2a8d2d954
shenxh <xianhui.shen@eslink.com>
2023-03-14 14:13:22 +0800

    feat: sass to less

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './assets/css/common.scss'
+import './assets/css/common.less'
 
 Vue.config.productionTip = false
 
