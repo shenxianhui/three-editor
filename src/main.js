78a006e5d8ded9b85b158fe2e96b274bf337d137
shenxh <xianhui.shen@eslink.com>
2023-03-14 18:58:31 +0800

    feat: 引入右键菜单插件

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import Contextmenu from 'vue-contextmenujs'
 
 import './assets/css/common.less'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(Contextmenu)
 
 new Vue({
   router,
--- src/views/three-editor/components/scene-menu.vue
+++ src/views/three-editor/components/scene-menu.vue
@@ -3,18 +3,19 @@
  * @Author: shenxh
  * @Date: 2023-03-14 16:48:37
  * @LastEditors: shenxh
- * @LastEditTime: 2023-03-14 16:57:22
+ * @LastEditTime: 2023-03-14 18:57:01
 -->
 
 <template>
   <div class="scene-menu">
-    <el-menu class="menu" :default-active="0">
+    <el-menu class="menu" default-active="0">
       <div class="menu-item-wrap">
         <el-menu-item
           class="menu-item"
           v-for="(item, index) in sceneList"
           :key="index"
-          :index="index"
+          :index="index + ''"
+          @contextmenu.prevent.native="onContextmenu($event, item, index)"
         >
           <div class="menu-item-group">
             <i class="el-icon-menu"></i>
@@ -61,7 +62,45 @@ export default {
       }
 
       this.sceneList.push(scene)
+
       this.$emit('add-scene', scene, this.sceneList)
+    },
+
+    // 删除场景
+    delScene(itm, idx) {
+      const { name = '' } = itm
+
+      this.$confirm(`是否确认删除"${name}"?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.sceneList.splice(idx, 1)
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          })
+
+          this.$emit('del-scene', itm, this.sceneList)
+        })
+        .catch(() => {})
+    },
+
+    // 右键场景
+    onContextmenu(event, itm, idx) {
+      this.$contextmenu({
+        items: [
+          {
+            label: '删除',
+            onClick: () => this.delScene(itm, idx),
+          },
+        ],
+        event,
+        customClass: 'custom-class',
+        zIndex: 3,
+        minWidth: 50,
+      })
     },
   },
 }
